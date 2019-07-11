1eb99b140f3c9cb22837021e0612334c3c69c97c
Aya Matsubara <mats0035@algonquinlive.com>
2019-07-11 11:41:51 -0400

    Added sort function

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $().ready(function () {
   // Library view: drag & drop function
   $('#library-view ol.sortable').on('click', function(event) {
-    // If the shift key is pressed => duplicate the element on drop
+    // If shift key is pressed => duplicate the element on drop
     if (event.shiftKey) {
       $(this).nestedSortable({
         forcePlaceholderSize: true,
@@ -26,7 +26,7 @@ $().ready(function () {
         excludeRoot: true,
         rootID: 'root'
       })
-    // If the shift key is not pressed => just move the element
+    // If shift key is not pressed => just move the element
     } else {
        $(this).nestedSortable({
         forcePlaceholderSize: true,
@@ -57,8 +57,8 @@ $().ready(function () {
     helper: function (e, li) {
         this.copyHelper = li.clone(true).insertAfter(li)
         $(this).data('copied', false)
-        // Return the copied li with 'add' class
-        return li.addClass('add').clone()
+        // Return the copied li with a flag ('copy' class)
+        return li.addClass('copy').clone()
     },
     items: 'li',
     opacity: 0.6,
@@ -78,11 +78,14 @@ $().ready(function () {
     // Run functions when drag & drop is finished
     stop: function (e, li) {
       // Delete the duplicated li in the competency view
-      $('#competency-view').find('.add').remove()
-      // Remove the flag('add' class) in the library view
-      $('#library-view').find('.add').removeClass('add')
+      $('#competency-view').find('.copy').remove()
+      // Remove the flag('copy' class) in the library view
+      $('#library-view').find('.copy').removeClass('copy')
     }
   })
+
+  // Trigger dtag & drop functions
+  $('#library-view ol.sortable').click()
 
   // Set events on the buttons
   $('.expandEditor').attr('title', 'Click to show/hide item editor')
@@ -112,33 +115,113 @@ $().ready(function () {
     $(this).parent().parent().remove()
   })
 
-  // Highlight the element when the star icon is clicked
+  // Highlight the element when a star icon is clicked
   $('.highlight').click(function () {
-    // Get the second class name of the parent li
-    var highlightClass = $(this).parent().parent().attr('class').split(' ')[1]
+    // Get the first class name (guid) of the parent li
+    var highlightClass = $(this).parent().parent().attr('class').split(' ')[0]
+    // Select all star icons under the same guid
     var highlightItem = '.' + highlightClass + ' > .menuDiv .glyphicon-star'
     $(highlightItem).toggleClass('highlight-show')
+  })
+
+  // Sort EOs by course name/course code
+  $('#sort-menu').change(function() {
+    let sortFlg   // 'name' or 'code'
+    let sortTitle = []  // EO titles to sort
+    const currentTitle = [] // Current EO titles (i.e. GIS4212 - PM Methods)
+    const sortedElement = []
+    const sortView = document.getElementById('competency-view')
+    const currentOl = sortView.getElementsByClassName('ol-level')  // 'ol' elements
+    const currentLis = sortView.getElementsByClassName('list-item') // 'li' elements
+    const sortTarget = sortView.getElementsByClassName('itemTitle')  // 'span' elements with 'itemTitle' class
+
+    // Function to sort elements of sortTitle array
+    function sortItems (sortFlg) {
+      sortTitle.sort(function(a, b) {
+        // If sort targets are course names
+        if (sortFlg === "name") {
+          a = a.toString().toLowerCase();
+          b = b.toString().toLowerCase();
+        }
+        // Sort elements of sortTitle array
+        if (a < b) {
+          return -1;
+        } else if (a > b){
+          return 1;
+        }
+        return 0; 
+      })        
+    }
+
+    // Get a value of sort menu: course code/course name
+    let value = $(this).val()
+    if (value === "Course code") {
+      sortFlg = "code"
+    } else if (value === "Course name") {
+      sortFlg = "name"
+    }
+
+    // Set array to sort
+    for (let i = 0; i < sortTarget.length; i++) {
+      // If sort by course name
+      if (sortFlg === "name") {
+        // Divide the EO into a code and title
+        let tmp = sortTarget[i].textContent.split('- ')
+        // If EO doesn't have a code
+        if (typeof(tmp[1]) == "undefined") {  
+          currentTitle.push(tmp[0])
+        } else {
+          currentTitle.push(tmp[1])
+        }
+      // If sort by course code
+      } else {
+        currentTitle.push(sortTarget[i].textContent)
+      }
+    }
+
+    // Duplicate currentTitle array
+    sortTitle = currentTitle.slice();
+
+    // Sort EOs in sortTitle array
+    sortItems(sortFlg)
+
+    // Set Lis in the order of sortTitle array
+    for (let j = 0; j < currentLis.length; j++) {
+      // Check if the current EO title matches with the new order
+      for (let k = 0; k < sortTitle.length; k++) {
+        // If matched, add the element in sortedElement array and break from loop
+        if (currentTitle[j] === sortTitle[k]) {
+          sortedElement[k] = currentLis[j]
+          break
+        }
+      }
+    }
+
+    // Replace the existing elements by sorted elements
+    for (let l = 0; l < sortedElement.length; l++) {
+      currentOl[0].appendChild(currentOl[0].removeChild(sortedElement[l]))
+    }
   })
 })
 
 // Function to search through list and hide the items that don't have the characters that were inputted
 function filterNames(view) {
-  itemView = document.getElementById(view)
+  const filterView = document.getElementById(view)
   // Get value of input
-  let filterValue = itemView.getElementsByClassName('filterInput')[0].value.toUpperCase()
+  const filterValue = filterView.getElementsByClassName('filterInput')[0].value.toUpperCase()
   // Get all EO elements
-  itemTitle = itemView.getElementsByClassName('itemTitle')
+  filterTitle = filterView.getElementsByClassName('itemTitle')
 
   // Loop through the EO elements
-  for (let i = 0; i < itemTitle.length; i++) {
+  for (let i = 0; i < filterTitle.length; i++) {
     // Get the EO title
-    const title = itemTitle[i].textContent.toUpperCase()
+    const title = filterTitle[i].textContent.toUpperCase()
     // If the EO title matched with the value of input, do nothing
     if (title.indexOf(filterValue) > -1) {
-      itemTitle[i].parentNode.style.display = ''
+      filterTitle[i].parentNode.style.display = ''
     // If the EO title didn't match with the value of input, hide the parent div
     } else {
-      itemTitle[i].parentNode.style.display = 'none' 
+      filterTitle[i].parentNode.style.display = 'none' 
     }
   }
 }
